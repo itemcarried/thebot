1ccbe438340f106f8bd8daa104766bb2650a10c8
itemcarried <20blapa@gmail.com>
2026-08-21 21:40:36 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -67,7 +67,7 @@ MAP_ROLES = {
 
 CHECK_INTERVAL = 60
 
-GIF_URL = " || https://tenor.com/view/bleach-nel-cutie-yes-yep-gif-18405016442900009995 || "
+GIF_URL = "https://tenor.com/view/bleach-nel-cutie-yes-yep-gif-18405016442900009995"
 
 intents = discord.Intents.default()
 intents.guilds = True
